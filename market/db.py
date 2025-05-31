b1783c462d6b27f95dcd84a768599203d93d895a
niapus <vladimir-evdokimov-1951@mail.ru>
2025-05-31 22:14:45 +0500

    2

--- market/db.py
+++ market/db.py
@@ -1,13 +1,12 @@
+import os
 from databases import Database
 from sqlalchemy import MetaData
+from dotenv import load_dotenv
 
-DB_HOST = "rc1d-0bvn2bd889klk39p.mdb.yandexcloud.net"
-DB_PORT = "6432"
-DB_NAME = "db1"
-DB_USER = "user1"
-DB_PASSWORD = "useradmin"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?ssl=require"
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
 
 database = Database(DATABASE_URL)
 metadata = MetaData()
